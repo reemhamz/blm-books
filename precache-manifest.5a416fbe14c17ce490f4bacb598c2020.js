9be6a98100f49bd3fb38244da15a74a8b5aee58a
Reem H <reem@fueled.com>
2020-06-10 17:19:40 -0400

    Updates

---
+++ precache-manifest.5a416fbe14c17ce490f4bacb598c2020.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8589d52ce2a85968e512c13bafe10767",
+    "url": "/blm-books/index.html"
+  },
+  {
+    "revision": "50e792a0062ff147a46a",
+    "url": "/blm-books/static/css/main.b5464898.chunk.css"
+  },
+  {
+    "revision": "1d89e5651b372f2c7acf",
+    "url": "/blm-books/static/js/2.e8fd0e11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blm-books/static/js/2.e8fd0e11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e792a0062ff147a46a",
+    "url": "/blm-books/static/js/main.3416f152.chunk.js"
+  },
+  {
+    "revision": "e70f339a97dd348ccd7f",
+    "url": "/blm-books/static/js/runtime-main.d736671d.js"
+  },
+  {
+    "revision": "eb065264c54650403942e2c059af43ae",
+    "url": "/blm-books/static/media/aint-i-a-woman.eb065264.jpg"
+  },
+  {
+    "revision": "04ba90d8d494841eb56c7d305bcac695",
+    "url": "/blm-books/static/media/bad-feminist.04ba90d8.jpg"
+  },
+  {
+    "revision": "0033b51a3958a6a485c31de98858b668",
+    "url": "/blm-books/static/media/between-the-world-and-me.0033b51a.jpg"
+  },
+  {
+    "revision": "b908d0f6219ab10e7a3009e164744b4f",
+    "url": "/blm-books/static/media/black-feminist-thought.b908d0f6.jpg"
+  },
+  {
+    "revision": "f3bd08bc0c1a4e564ea67914079547c3",
+    "url": "/blm-books/static/media/blackballed.f3bd08bc.jpg"
+  },
+  {
+    "revision": "6487dc7a55505c30932b4eeab15db965",
+    "url": "/blm-books/static/media/citizen.6487dc7a.jpg"
+  },
+  {
+    "revision": "d674d9fec57ad838df513e1c67ef46f7",
+    "url": "/blm-books/static/media/dying-of-whiteness.d674d9fe.jpg"
+  },
+  {
+    "revision": "39248a63836211d0a3f7d9764ba75ff0",
+    "url": "/blm-books/static/media/eloquent-rage.39248a63.jpg"
+  },
+  {
+    "revision": "806d7832e1ee532245a1ba2ffd0b0043",
+    "url": "/blm-books/static/media/evicted.806d7832.jpg"
+  },
+  {
+    "revision": "6f11fbf580b84ef834f1299f15f429f0",
+    "url": "/blm-books/static/media/fearing-the-black-body.6f11fbf5.jpg"
+  },
+  {
+    "revision": "0afb8be11fe3d25fe0d20ff4bf160352",
+    "url": "/blm-books/static/media/how-to-be-an-anti-racist.0afb8be1.jpg"
+  },
+  {
+    "revision": "3d384b75443ca4817309cb0aec2e084c",
+    "url": "/blm-books/static/media/how-we-get-free.3d384b75.jpg"
+  },
+  {
+    "revision": "68948918ca9990cd5f5e8f540eac4d69",
+    "url": "/blm-books/static/media/i-know-why-the-caged-bird-sings.68948918.jpg"
+  },
+  {
+    "revision": "4ddde486903883311889fea496a7ed8f",
+    "url": "/blm-books/static/media/im-still-here.4ddde486.jpg"
+  },
+  {
+    "revision": "309de696f7fa90f22f0f35deda6fa917",
+    "url": "/blm-books/static/media/killing-rage.309de696.jpg"
+  },
+  {
+    "revision": "ee9e3b53c10d4336d10848299ffa8893",
+    "url": "/blm-books/static/media/lies-my-teacher-told-me.ee9e3b53.jpg"
+  },
+  {
+    "revision": "3dc741b4cf348a1bf841797f20eb4c2a",
+    "url": "/blm-books/static/media/malcon-x.3dc741b4.jpg"
+  },
+  {
+    "revision": "2717414077f48cdbf0d636a85e542eb7",
+    "url": "/blm-books/static/media/me-and-white-supremecy.27174140.jpg"
+  },
+  {
+    "revision": "2c70dbdf8531402f8560bdaf8d242ac1",
+    "url": "/blm-books/static/media/medical-apartheid.2c70dbdf.jpg"
+  },
+  {
+    "revision": "06ee6e19ccd3110ba0f1415a78151175",
+    "url": "/blm-books/static/media/no-shade-no-tea.06ee6e19.jpg"
+  },
+  {
+    "revision": "5565277bfc6579b368aea43d5b953584",
+    "url": "/blm-books/static/media/nobody.5565277b.jpg"
+  },
+  {
+    "revision": "f2ada3a6a533b48f1293652ae4054f29",
+    "url": "/blm-books/static/media/sister-outside.f2ada3a6.jpeg"
+  },
+  {
+    "revision": "4457c25f6e27c07914ed11161cf2f488",
+    "url": "/blm-books/static/media/so-you-want-to-talk-about-race.4457c25f.jpg"
+  },
+  {
+    "revision": "381b65b2c049574f483390b4f186c9d9",
+    "url": "/blm-books/static/media/stamped-from-the-beginning.381b65b2.jpg"
+  },
+  {
+    "revision": "801d5ce53ba3750250b94a5918b0ea76",
+    "url": "/blm-books/static/media/the-bluest-eye.801d5ce5.jpg"
+  },
+  {
+    "revision": "53357d5fee62e49927b995e1ef2ef506",
+    "url": "/blm-books/static/media/the-bridge-called-my-back.53357d5f.jpg"
+  },
+  {
+    "revision": "0d6a5b3fa90018ea6f394f2352a0ae47",
+    "url": "/blm-books/static/media/the-burning-house.0d6a5b3f.jpg"
+  },
+  {
+    "revision": "81422fe985fc498b07d347c00bb0ca64",
+    "url": "/blm-books/static/media/the-color-of-law.81422fe9.jpg"
+  },
+  {
+    "revision": "34c8a403fbb99ea7a25d2f07615ccf58",
+    "url": "/blm-books/static/media/the-condemnation-of-blackness.34c8a403.jpg"
+  },
+  {
+    "revision": "a584480fae9d3b54631b63d05d9e8080",
+    "url": "/blm-books/static/media/the-fire-next-time.a584480f.jpg"
+  },
+  {
+    "revision": "55f048e6b07abaf211c743df4462472b",
+    "url": "/blm-books/static/media/the-hate-u-give.55f048e6.jpg"
+  },
+  {
+    "revision": "ba1685d994e9b05616ebfd97125f2229",
+    "url": "/blm-books/static/media/the-new-jim-crow.ba1685d9.jpg"
+  },
+  {
+    "revision": "0a1bc9cec5113024ae51375de221806e",
+    "url": "/blm-books/static/media/the-warmth-of-other-suns.0a1bc9ce.jpg"
+  },
+  {
+    "revision": "9e9f5be35b8d9010ba62135d76cf3098",
+    "url": "/blm-books/static/media/the-wretched-of-the-earth.9e9f5be3.jpg"
+  },
+  {
+    "revision": "8453c53ccae120303538ebe22822e625",
+    "url": "/blm-books/static/media/unapologetic.8453c53c.jpg"
+  },
+  {
+    "revision": "1b9df9fc829239bab42fe735468ba4e7",
+    "url": "/blm-books/static/media/when-they-call-you-a-terrorist.1b9df9fc.jpg"
+  },
+  {
+    "revision": "197b6358b5065a80802d7435edec1d33",
+    "url": "/blm-books/static/media/white-fragility.197b6358.jpg"
+  },
+  {
+    "revision": "3bf3dfa592f5284562b634ecff8488ca",
+    "url": "/blm-books/static/media/white-rage.3bf3dfa5.jpg"
+  },
+  {
+    "revision": "441d3f12cbe6cdb2b7f91d7e3ba75908",
+    "url": "/blm-books/static/media/why-im-no-longer-talking-to-white-people-about-race.441d3f12.jpg"
+  },
+  {
+    "revision": "1d167d724f139f2383d6a602c9fa1ea3",
+    "url": "/blm-books/static/media/women-race-and-class.1d167d72.jpg"
+  }
+]);
